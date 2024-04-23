e2a82d1f99772c89db936ba74c7135057252efd6
pankajm1995 <5006pankajmaurya@gmail.com>
2024-04-24 04:30:01 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Fiction from "./component/Fiction";
+
+import './App.css'
+import NonFiction from "./component/NonFiction";
+import { useState } from "react";
 
 function App() {
+  const [value,setvalue]=useState(true)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="App" style={{color:"tomato"}}>Mini Book Store</h1>
+
+      <button  onClick={()=>setvalue(!value)} style={{height:45}} ><h2>{value?"show finctional Book":"show not finctional book" }</h2></button>
+
+      <div className="App">
+        {/* Render either Fiction or NonFiction Based on the Condition */}
+        {value?<Fiction/>:<NonFiction/>}
+
+
+      </div>
     </div>
   );
 }
