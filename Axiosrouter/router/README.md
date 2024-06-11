039bf311e9542e1ff3131070f4694cbc8c763b6e
pankajk1995 <5006pankajkmaurya@gmail.com>
2024-06-12 02:50:22 +0530

    update

--- Axiosrouter/router/README.md
+++ Axiosrouter/router/README.md
@@ -1,3 +1,5 @@
+
+https://drive.google.com/file/d/1jT6lUlLQa2fXP6C1JZqrObL7hoTjHVBK/view?usp=sharing
 ```
 Routing with private router
 1. create component
--- Axiosrouter/router/src/pages/Login.jsx
+++ Axiosrouter/router/src/pages/Login.jsx
@@ -1,41 +1,51 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+function Login() {
+    const [email, setemail] = useState("")
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate();
 
-const Login = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const userdata = { email, password }
+        console.log(userdata)
+        fetch("https://reqres.in/api/login", {
 
-    const [email,setemail]=useState('')
-    const [password,setpass] = useState('')
-
-    const handlesubmit=(e)=>
-        {
-                e.preventDefault()
-                let userdata={
-                    email,password
-                }
-                console.log(userdata)
-
-                fetch("https://reqres.in/api/login",{
-                    method:"POST",
-                    headers:{
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(userdata)
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userdata)})
+            .then((res) => res.json())
+                .then((data) => {
+                    if (data.token) {
+                        localStorage.setItem("token", data.token);
+                        document.getElementById("msg").textContent ="Login Successfully"
+                        navigate("/product")
+                    } else {
+                        localStorage.removeItem("token");
+                        document.getElementById("msg").textContent ="Invalid user"
+                    }
                 })
-                .then((res)=>res.json())
-                .then((data)=>{
-                    localStorage.setItem("token",data.token)
-                })
-                .catch((err)=>console.log(err))
-        }
-  return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handlesubmit}>
-        <input type='email' placeholder='enter email' onChange={(e)=>setemail(e.target.value)}></input>
-        <input type='password' placeholder='enter password' onChange={(e)=>setpass(e.target.value)}></input>
-        <input type='submit'></input>
-      </form>
-    </div>
-  )
+                .catch((err) => console.log(err))
+    }
+
+
+
+    return (
+        <div>
+            <form action="" onSubmit={handleSubmit} >
+                <h1>Login Here</h1>
+                <input className='log' type="text" onChange={(e) => setemail(e.target.value)} placeholder='Enter Email...' />
+                <br />
+                <input className='log' type="text" onChange={(e) => setPassword(e.target.value)} placeholder='Password Email...' />
+                <br />
+                <input type="Submit" variant="outline-primary" />
+                <h3 id='msg'></h3>
+            </form>
+        </div>
+
+    )
 }
 
 export default Login
