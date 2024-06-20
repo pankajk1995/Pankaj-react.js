f3140400a85142422e14e478474f16434fa1384f
pankajk1995 <5006pankajkmaurya@gmail.com>
2024-06-20 23:51:59 +0530

    update

--- Redux Builder/Day5/Crud/src/Redux/Product/reducer.js
+++ Redux Builder/Day5/Crud/src/Redux/Product/reducer.js
@@ -1,39 +1,40 @@
-import { GET_PRODUCT_FAILURE, GET_PRODUCT_REQUEST, GET_PRODUCT_SUCCESS, POST_PRODUCT_FAILURE, POST_PRODUCT_REQUEST, POST_PRODUCT_SUCCESS } from "../actionType"
+import {
+  GET_PRODUCT_FAILURE,
+  GET_PRODUCT_REQUEST,
+  GET_PRODUCT_SUCCESS,
+  POST_PRODUCT_FAILURE,
+  POST_PRODUCT_REQUEST,
+  POST_PRODUCT_SUCCESS,
+} from "../actionType";
 
-let initialstate={
-    isLoading:false,
-    isError:false,
-    data:[],
-    totalpage:1
-}
-export const reducer =(state=initialstate,{type,payload})=>{
-    switch(type){
-        case POST_PRODUCT_REQUEST:
-            {
-                return{...state,isLoading:payload}
-            }
-            case POST_PRODUCT_SUCCESS:
-                {
-                    return{...state}
-                }
-                case POST_PRODUCT_FAILURE:
-                    {
-                        return{...state,isError:payload}
-                    }
-                    // data get process
-                    case GET_PRODUCT_REQUEST:
-                        {
-                            return{...state,isLoading:payload}
-                        }
-                        case GET_PRODUCT_SUCCESS:
-                            {
-                                return{...state,data:payload.data,totalpage:payload.page}
-                            }
-                            case GET_PRODUCT_FAILURE:
-                                {
-                                    return{...state,isError:payload}
-                                }
-        default:
-            return state
+let initialstate = {
+  isLoading: false,
+  isError: false,
+  data: [],
+  totalpage: 1,
+};
+export const reducer = (state = initialstate, { type, payload }) => {
+  switch (type) {
+    case POST_PRODUCT_REQUEST: {
+      return { ...state, isLoading: payload };
     }
-}
+    case POST_PRODUCT_SUCCESS: {
+      return { ...state };
+    }
+    case POST_PRODUCT_FAILURE: {
+      return { ...state, isError: payload };
+    }
+    // data get process
+    case GET_PRODUCT_REQUEST: {
+      return { ...state, isLoading: payload };
+    }
+    case GET_PRODUCT_SUCCESS: {
+      return { ...state, data: payload.data, totalpage: payload.page };
+    }
+    case GET_PRODUCT_FAILURE: {
+      return { ...state, isError: payload };
+    }
+    default:
+      return state;
+  }
+};
--- Redux Builder/Day5/Crud/src/component/Productdata.jsx
+++ Redux Builder/Day5/Crud/src/component/Productdata.jsx
@@ -1,85 +1,176 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { GET_PRODUCT_FAILURE, GET_PRODUCT_REQUEST, GET_PRODUCT_SUCCESS } from '../Redux/actionType'
-import { Link, useSearchParams } from 'react-router-dom'
-import { GetproductData } from '../Redux/Product/action'
-import { BsSearchHeartFill } from "react-icons/bs";
-const Productdata = () => {
-    const dispatch=useDispatch()
-    const {isLoading,isError,data,totalpage}=useSelector((store)=>store.ProductReducer)
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-  const [search,setsearch]=useSearchParams()
- console.log(totalpage)
+import { Link, useSearchParams } from "react-router-dom";
+import { GetproductData } from "../Redux/Product/action";
+
+const Productdata = () => {
+  const dispatch = useDispatch();
+  const { isLoading, isError, data, totalpage } = useSelector(
+    (store) => store.ProductReducer
+  );
+
+  const [search, setsearch] = useSearchParams();
+  console.log(totalpage);
   // for pagination state
 
-  const [page,setpage] =useState(1)
-// for searching
+  const [page, setpage] = useState(1);
+  // for searching
 
-const [find,setfind]=useState("")
-    useEffect(()=>{
-      let paramobj={
-    
-        params:{
-          category:search.getAll("category"),
-          _sort:"price",
-          _order:search.get("sort"),
-          _limit:6,
-          _page:page,
-          q:find
-        }
-       
-     }
-    let id= setTimeout(() => {
-      dispatch(GetproductData(paramobj))
+  const [find, setfind] = useState("");
+  useEffect(() => {
+    let paramobj = {
+      params: {
+        category: search.getAll("category"),
+        _sort: "price",
+        _order: search.get("sort"),
+        _limit: 6,
+        _page: page,
+        q: find,
+      },
+    };
+    let id = setTimeout(() => {
+      dispatch(GetproductData(paramobj));
     }, 1000);
-    return ()=>{
+    //clear time use for prevent refresh
+    return () => {
       console.log("clean up");
-      clearTimeout(id)
-    }
-       
-    },[search,page,find])
+      clearTimeout(id);
+    };
+  }, [search, page, find]);
 
- 
-// Delete part
-    const handledelete=(id)=>{
-      axios.delete(`http://localhost:8080/product/${id}`).then((res)=>{
-        dispatch(GetproductData({}))
-        console.log(res)})
-      .catch((err)=>console.log(err))
-    }
+  // Delete part
+  const handledelete = (id) => {
+    axios
+      .delete(`http://localhost:8080/product/${id}`)
+      .then((res) => {
+        dispatch(GetproductData({}));
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  };
 
-    // update 
+  // update
 
-    
-  return isLoading ? <h1>Loading..</h1>:isError?<h1>Some thing went wrong</h1>:(
+  return isLoading ? (
+    <h1>Loading..</h1>
+  ) : isError ? (
+    <h1>Some thing went wrong</h1>
+  ) : (
     <div>
-        <div>
-        < input value={find} onChange={(e)=>setfind(e.target.value)} type="text" placeholder="search here"  />
+      <div>
+        <input
+          value={find}
+          onChange={(e) => setfind(e.target.value)}
+          type="text"
+          placeholder="search here"
+        />
       </div>
-    <div style={{display:'grid',gridTemplateColumns:"repeat(3,1fr)",gap:'15px',marginTop:"15px"}}>
-      {data.length>0 && data.map((el)=>(<div key={el.id} style={{border:"2px solid green" }}>
-        <div style={{textAlign:'center' ,boxshadow: "rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px"}}>
-        {<img src={el.image} alt="" height={200} width={200}/>}
-        <h3 style={{color:"green"}}>{el.category}</h3>
-        <marquee><p>💲:{el.price}</p></marquee>
-        <p>{el.description}</p>
-        <div style={{padding:"5px" ,margin:"3px"}}>
-        <button onClick={()=>handledelete(el.id)} style={{height:"35px",color:"red",backgroundColor:"black", cursor:"pointer" }}>Delete</button>
-       <Link to={`/edit/${el.id}`}><button style={{height:"35px",color:"red",backgroundColor:"black", cursor:"pointer" }}>Edit</button></Link> 
-       <Link to={`/editall/${el.id}`}><button style={{height:"35px",color:"red",backgroundColor:"black", cursor:"pointer" }}>Edit all data</button></Link>
-       </div>
-        </div>
-      </div>))}
-    </div>
-    <div style={{marginTop:"25px",textAlign:"center"}}>
-      <button  style={{height:"35px",color:"yellow",backgroundColor:"black", cursor:"pointer" }} disabled={page==1} onClick={()=>setpage(page-1)}>Pre</button>
-      <button style={{height:"35px",color:"yellow",backgroundColor:"black", cursor:"pointer" }}>{page}</button>
-      <button style={{height:"35px",color:"yellow",backgroundColor:"black", cursor:"pointer" }} disabled={page==Math.ceil(totalpage/5)} onClick={()=>setpage(page+1)}>Next</button>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3,1fr)",
+          gap: "15px",
+          marginTop: "15px",
+          backgroundColor:"grey"
+        }}
+      >
+        {data.length > 0 &&
+          data.map((el) => (
+            <div key={el.id} style={{ border: "2px solid green" }}>
+              <div
+                style={{
+                  textAlign: "center",
+                  boxshadow:
+                    "rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px",
+                }}
+              >
+                {<img src={el.image} alt="" height={200} width={200} style={{marginTop:"12px",backgroundColor:"transparent"}}/>}
+                <h3 style={{ color: "green" }}>{el.category}</h3>
+                <marquee>
+                  <p>💲:{el.price}</p>
+                </marquee>
+                <p>{el.description}</p>
+                <div style={{ padding: "5px", margin: "3px" }}>
+                  <button
+                    onClick={() => handledelete(el.id)}
+                    style={{
+                      height: "35px",
+                      color: "red",
+                      backgroundColor: "black",
+                      cursor: "pointer",
+                    }}
+                  >
+                    Delete
+                  </button>
+                  <Link to={`/edit/${el.id}`}>
+                    <button
+                      style={{
+                        height: "35px",
+                        color: "red",
+                        backgroundColor: "black",
+                        cursor: "pointer",
+                      }}
+                    >
+                      Edit
+                    </button>
+                  </Link>
+                  <Link to={`/editall/${el.id}`}>
+                    <button
+                      style={{
+                        height: "35px",
+                        color: "red",
+                        backgroundColor: "black",
+                        cursor: "pointer",
+                      }}
+                    >
+                      Edit all data
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+      <div style={{ marginTop: "25px", textAlign: "center" }}>
+        <button
+          style={{
+            height: "35px",
+            color: "yellow",
+            backgroundColor: "black",
+            cursor: "pointer",
+          }}
+          disabled={page == 1}
+          onClick={() => setpage(page - 1)}
+        >
+          Pre
+        </button>
+        <button
+          style={{
+            height: "35px",
+            color: "yellow",
+            backgroundColor: "black",
+            cursor: "pointer",
+          }}
+        >
+          {page}
+        </button>
+        <button
+          style={{
+            height: "35px",
+            color: "yellow",
+            backgroundColor: "black",
+            cursor: "pointer",
+          }}
+          disabled={page == Math.ceil(totalpage / 5)}
+          onClick={() => setpage(page + 1)}
+        >
+          Next
+        </button>
       </div>
     </div>
-   
-  )
-}
+  );
+};
 
-export default Productdata
+export default Productdata;
